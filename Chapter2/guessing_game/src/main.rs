547b39e2f9e428485c6db79f30141f6d0154010b
yotsugi <harukipdr@gmail.com>
2020-09-20 13:40:11 +0900

    chapter2 fin

--- Chapter2/guessing_game/src/main.rs
+++ Chapter2/guessing_game/src/main.rs
@@ -1,21 +1,34 @@
 extern crate rand;
 
-use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    loop {
+        println!("please input your guess");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("please input your guess");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("you guessed: {}", guess);
+        println!("you guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+        }
+    }
 }
--- Chapter2/memo.md
+++ Chapter2/memo.md
@@ -1,2 +1,4 @@
 rustの変数は標準でimmutable  
 mutableにするには変数名の前に`mut`を付ける
+
+match式がようわからん
