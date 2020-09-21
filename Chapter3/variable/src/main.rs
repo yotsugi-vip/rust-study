96399458dc73a0088fba02e4e5940a7330a97bbf
yotsugi <harukipdr@gmail.com>
2020-09-21 23:24:01 +0900

    teiki

--- Chapter3/variable/src/main.rs
+++ Chapter3/variable/src/main.rs
@@ -1,3 +1,8 @@
+const MAX_POINTS: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("the value of x is {}", x);
+    x = 6;
+    println!("the value of x is {}", x);
 }
