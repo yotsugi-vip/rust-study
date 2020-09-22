ecb52cb5b7f9a92fdf9a75d4cfb744f28749298a
yotsugi <harukipdr@gmail.com>
2020-09-22 20:53:06 +0900

    teiki

--- Chapter3/variable/src/main.rs
+++ Chapter3/variable/src/main.rs
@@ -6,3 +6,23 @@ fn main() {
     x = 6;
     println!("the value of x is {}", x);
 }
+
+fn shadowing() {
+    let space = "   ";
+    // shadowing
+    let space = space.len();
+
+    // mutでの型変更はできない
+    //let mut space = "   ";
+    //space = space.len();
+}
+
+fn data_type() {
+    // tuple
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let tuptup = (100, "aiu", 8.5);
+    let (x, y, z) = tuptup;
+    let a = tuptup.0;
+    let b = tuptup.1;
+    println!("the value y is {}", y);
+}
